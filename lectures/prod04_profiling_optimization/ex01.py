0e47950ece856a048384209a351900f1aaf05640
jetminded <a>
2026-04-18 14:15:21 +0300

    lec1804

--- lectures/prod04_profiling_optimization/ex01.py
+++ lectures/prod04_profiling_optimization/ex01.py
@@ -1,4 +1,13 @@
-def fibonacci(n):
+import cProfile
+import pstats
+import time
+
+def foo(x, y):
+    time.sleep(0.05)
+    return 2 * x + 2 * y
+
+
+def fibonacci(n, foo):
     if n <= 0:
         return []
     elif n == 1:
@@ -7,10 +16,20 @@ def fibonacci(n):
     fib_list = [0, 1]
 
     for i in range(2, n):
-        fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+        time.sleep(0.05)
+        fib_list.append(foo(fib_list[i - 1], fib_list[i - 2]))
 
     return fib_list
 
+
+with cProfile.Profile() as pr:
+    fibonacci(50, foo)
+
+res = pstats.Stats(pr)
+res.sort_stats(pstats.SortKey.TIME)
+res.print_stats()
+res.dump_stats("res.prof")
+# print(end - start) 
 
 # timeit
 # cprofile
